efca5d14fdb09bbe554424f97ec1716282f25d72
TITAS MALLICK <44458514+titasmallick@users.noreply.github.com>
2024-12-20 09:21:17 +0530

    starter

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <h1 className="font-mono">Start building from here 001</h1>
+    </section>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Building Block Titas",
+  description: "Titas always starts building from here.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+  },
+};
